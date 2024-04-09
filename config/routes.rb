6a6dde63b4a4a9823d5e0042b25d55498b56f2cc
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-08 21:41:23 -0500

    Created method new

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show'
 end
